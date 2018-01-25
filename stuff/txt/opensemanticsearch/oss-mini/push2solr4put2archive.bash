b886de1ad00bfe949042be05ab6063712fb81639
MÄRTEN RODES <marten.rodes@politsei.ee>
2018-01-25 11:51:28 +0200

    Revert "some basic error handling"
    This reverts commit 22fa75f9165dc86f466c09a6149975113652dcb4.

--- stuff/txt/opensemanticsearch/oss-mini/push2solr4put2archive.bash
+++ stuff/txt/opensemanticsearch/oss-mini/push2solr4put2archive.bash
@@ -5,39 +5,33 @@
 
 SOLR=""
 ARCHIVEDIR="/tmp/archive"
-
-log() { echo "$(date) $0: $*"; }
-error() { echo "$(date) $0: $*" >&2; }
-die() { error "$*"; exit 1; }
-
-[ -d $ARCHIVEDIR ] || mkdir -p $ARCHIVEDIR || die "can not create $ARCHIVEDIR"
-sleep 1
-log "starting with $1"
+[ -d $ARCHIVEDIR ] || mkdir -p $ARCHIVEDIR
+sleep 2
 cd $1
 tmp=$(mktemp -d)
 ls | grep -v meta.json | while read f; do
   md5=$(md5sum "$f"| cut -f1 -d" ")
-  #TODO query solr first for md5
-  # copy file and meta to tmp dir
   mkdir $tmp/$md5
   mv "$f" $tmp/$md5/
+  #file "$tmp/$md5/$f"
   cp meta.json $tmp/$md5/
   cd $tmp/$md5
-  # index file
-  etl-file $(pwd) |& while read m; do log $m; done
-  # pack it up
-  TMP=$(mktemp)
+  pwd
   # TODO if exists $ARCHIVEDIR/$md5.tar.xz
-  tar -cv * | xz -9v -e > $ARCHIVEDIR/$md5.tar.xz 2> $TMP
-  last=$?
-  [ $last != 0 ] && die "xz error on $md5 $f $1 $(cat $TMP)"
+  etl-file --verbose $(pwd)
+  tar -cv * | xz -9v > $ARCHIVEDIR/$md5.tar.xz
+  tar -cv * | gzip -9 > $ARCHIVEDIR/$md5.tar.gz
   rm *
   cd ..
   rmdir $tmp/$md5
   cd $1
-  log "archived  $f -> $tmp/$md5 -> $ARCHIVEDIR/$md5.tar.xz"
-done || die "some errors above "
+  pwd
+
+
+  echo "archived  $f -> $tmp/$md5 -> $ARCHIVEDIR/$md5.tar.xz"
+  ls $ARCHIVEDIR/$md5.tar.xz
+done
 rm meta.json
 cd ..
 rmdir $1
-log "done with $1"
+echo "done with $1"
